253bba298e482221ad5005893061ee1f07f6d6a9
Luis Alvarez <lealvarezp@unac.edu.co>
2020-05-10 21:44:27 -0500

    finalizacion del codigo

--- proyectoJava/Class.java
+++ proyectoJava/Class.java
@@ -1,75 +1,90 @@
+
 import java.util.Scanner;
 
 public class Class {
 
     public static void main(String[] args) {
 
-        Scanner leer = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Señor usurio por favor elija la moneda a compartir");
-        System.out.println("1.peso colombiano");
-        System.out.println("1.peso venezolano");
-        System.out.println("1.peso uruguayo");
-        System.out.println("1.peso mexicano");
-        System.out.println("1.peso sudafricano");
-        System.out.println("1.peso zuiso");
+        System.out.println("Señor usurio por favor digite el número de la moneda a convertir");
+        System.out.println("1 = Peso colombiano");
+        System.out.println("2 = Bolivar venezguayo");
+        System.out.println("4 = Peso mex Kong");
+        System.out.println("8 = Yuan ");
+        System.out.println("5 = Euro");
+        System.out.println("6 = Libra esterlina");
 
+        final Integer opc_money = sc.nextInt();
+
+        System.out.println("Ingrese la cantidad de divisa");
+        double quantity = sc.nextDouble();
+
+        switch (opc_money) {
+            case 1:
+                System.out.println(Exchange.colombianPesoToDollar(quantity));
+                break;
+            case 2:
+                System.out.println(Exchange.venezuelanBolivarToDollar(quantity));
+                break;
+            case 3:
+                System.out.println(Exchange.mexicanPesoToDollar(quantity));
+                break;
+            case 4:
+                System.out.println(Exchange.mexicanPesoToDollar(quantity));
+                break;
+            case 5:
+                System.out.println(Exchange.chinesseYuanToDollar(quantity));
+                break;
+        }
+        sc.close();
     }
 }
 
 class Exchange {
 
-    private static DolarRate dolarRate;
-
     public static double venezuelanBolivarToDollar(double bolivaresAmount) {
 
-        return dolarRate.venezuelanBolivar * bolivaresAmount;
+        return DolarRate.venezuelanBolivar * bolivaresAmount;
 
     }
 
     public static double colombianPesoToDollar(double pesosAmount) {
 
-        return dolarRate.colombianPeso * pesosAmount;
-
-    }
-
-    public static double uruguayPesoToDollar(double uruguayPeso) {
-
-        return dolarRate.uruguayPeso * uruguayPeso;
+        return DolarRate.colombianPeso * pesosAmount;
 
     }
 
     public static double mexicanPesoToDollar(double mexicanPeso) {
 
-        return dolarRate.mexicanPeso * mexicanPeso;
+        return DolarRate.mexicanPeso * mexicanPeso;
 
     }
 
-    public static double sudafricanRandToDollar(double sudafricanRand) {
+    public static double chinesseYuanToDollar(double chinesseYuan) {
 
-        return dolarRate.sudafricanRand * sudafricanRand;
-
-    }
-
-    public static double suizaFrancoToDollar(double suizaFranco) {
-
-        return dolarRate.suizaFranco * suizaFranco;
+        return DolarRate.chinesseYuan * chinesseYuan;
 
     }
 
+    public static double euroToDollar(double euro) {
+
+        return DolarRate.euro * euro;
+
+    }
+
+    public static double sterlinPoundToDollar(double sterlinPound) {
+
+        return DolarRate.sterlinPound * sterlinPound;
+
+    }
 }
 
 class DolarRate {
     public static final double venezuelanBolivar = 33081.0357;
     public static final double colombianPeso = 3958.414;
-    public static final double uruguayPeso = 42.55;
     public static final double mexicanPeso = 24.4;
-    public static final double sudafricanRand = 18.76;
-    public static final double suizaFranco = 0.97;
-    public static final double hongKongDollar = 7.7527;
     public static final double chinesseYuan = 7.1046519654;
-    public static final double polishZloty = 4.2157366;
-    public static final double israelShekel = 3.6864960;
     public static final double euro = 0.92626;
     public static final double sterlinPound = 0.809958;
 
